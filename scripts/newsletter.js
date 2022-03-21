98349914221ff721eaeb342d243ff9b72b0d0036
rkentg <rkentg20@gmail.com>
2022-03-21 12:01:41 +0800

    newsletterupdate

---
+++ scripts/newsletter.js
@@ -0,0 +1,14 @@
+
+function validateForm() {
+if (!validateEmail(document.getElementById('data_3').value.trim())) {
+alert('Email must be a valid email address!');
+return false;
+}
+return true;
+}
+function isEmpty(str) { return (str.length === 0 || !str.trim()); }
+function validateEmail(email) {
+var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
+return isEmpty(email) || re.test(email);
+}
+
